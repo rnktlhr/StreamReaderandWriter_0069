ff01bb904d4888524d6dd692f257dda6221dcb63
rnktlhr <rinakitluhur10@gmail.com>
2024-06-03 15:04:42 +0700

    membuat program untuk penanganan pengecualian dengan manual exeception yang dimana program tersebut membantu agar program terus berjalan hingga akhir (tidak heng)

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+int main(){
+    try{
+        cout << "Selmat Belajar di Prodi TI UMY" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch(int a) {
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
+    catch(...){
+        cout << "Default Pengecualian diexsekusi" << endl;
+    }
+    return 0;
+}
