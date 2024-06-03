f7c534e23334d5beb26542d39df6bc0045c8bfdd
rnktlhr <rinakitluhur10@gmail.com>
2024-06-03 14:49:46 +0700

    menambHKn looping untuk menulis ytang didalamnya terdapat getline untuk mendapatkan karakter setiap saatu baris  dan  iff baris = q untuk menghentikan loop

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -14,4 +14,15 @@ int main(){
     ofstream outfile;
     //menunjuk kesebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << " >=Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true){
+        cout << "- ";
+        //mendapatkan karakter setiap satu baris
+        getline(cin, baris);
+        //loop akan berenti jika anda memasukan karakter q
+        if (baris == "q") break;
+    }
 }
