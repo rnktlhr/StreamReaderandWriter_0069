6a16f296854cf9b197cfa29581e71557913fd164
rnktlhr <rinakitluhur10@gmail.com>
2024-06-03 14:32:51 +0700

    menambahkan infile open untuk menunjuk sebuah file

--- ioFile.cpp
+++ ioFile.cpp
@@ -29,4 +29,9 @@ int main(){
 
     //membuka file dalam mode baca
     ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
 }
