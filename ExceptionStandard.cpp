4d6693bc6201b027c18720a06c736d5fcf52ff21
rnktlhr <rinakitluhur10@gmail.com>
2024-06-03 15:18:57 +0700

    melengkapi codingan dengan coment

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -4,15 +4,19 @@
 using namespace std;
 
 int main(){
-    cout << "Awal Program" << endl;
+    cout << "Awal Program" << endl; //penanda 1:....
     try{
         array<int, 4> data = {8, 4, 7, 2};
+        //pesan array int 4 elemen
         cout << data.at(5) << endl;
     }
 
     catch (exception& e) {
+        //penangkap menggunakan obyek exception
         cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 4 elemen*/
     }
     cout << "Baris Program Yang Terakhir" << endl;
-    
+    //penanda 2 : bahwa program berjalan tanpa berenti
+    return 0;
 }
